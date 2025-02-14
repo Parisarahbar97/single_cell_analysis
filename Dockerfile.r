25fe925bfdeeb369e775eff6cfb635ea49699bb8
Parisarahbar97 <parisa.rahbar22@imperial.ac.uk>
2025-02-14 15:19:16 +0000

    Update Dockerfile.r

--- Dockerfile.r
+++ Dockerfile.r
@@ -3,7 +3,7 @@ FROM r-base:latest
 # Set the working directory in the container
 WORKDIR /app
 
-# Install system dependencies for R
+# Install system dependencies for R including git
 RUN apt-get update && apt-get install -y --no-install-recommends \
     libssl-dev \
     libcurl4-openssl-dev \
@@ -16,8 +16,11 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     curl \
     wget \
     git \
-    build-essential \
-    && rm -rf /var/lib/apt/lists/*
+    build-essential && \
+    rm -rf /var/lib/apt/lists/*
+
+# Clone a GitHub repository (replace 'username' and 'repository' with your details)
+RUN git clone https://github.com/Parisarahbar97/single_cell_analysis.git /app
 
 # Install R packages
 RUN R -e "install.packages(c('ggplot2', 'dplyr', 'readr', 'BiocManager', 'Seurat'), repos='http://cran.rstudio.com')" \
